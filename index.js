ee1fe0cd35aa7802b90b223a59564718de44f5d2
Frank Meeuwsen <frank@incredibleadventure.nl>
2018-04-21 22:02:51 +0200

    update feedread and opml stuff

--- index.js
+++ index.js
@@ -1,42 +1,75 @@
-var Pinboard = require('node-pinboard');
-var api_token = process.env.pinboard;
-var pinboard = new Pinboard(api_token);
+const fs = require("fs");
+const request = require("request");
+const utils = require ("daveutils");
+const feedRead = require ("davefeedread");
+const opmlRead = require("daveopml");
 
+const timeOutSecs = 30;
+const opmlTestfeed = "./data/feeds.opml";
+const itemsToPostFile = "./data/itemsToPost.json";
+const itemArray = [];
 
-	var options = {
-		tags: 'microlink',
-   };
-  //
-	// pinboard.add(options, function(err, res) {
-  //   	console.log(res);
-  //   	//{ result_code: 'done' }
-	// });
-	// pinboard.recent({tag: 'microlink'}, function(err, res) {
-	// 	console.log(res);
-	// 		//date: date,
-  // 			//user: 'user',
-  // 			//posts:
-  //  			//[ { href: 'https://github.com/mikeal/request',
-  //     		//description: 'mikeal / request',
-  //     		//extended: '',
-  //     		//meta: 'meta',
-  //     		//hash: 'hash',
-  //     		//time: 'time',
-  //     		//shared: 'no',
-  //     		//toread: 'yes',
-  //     		//tags: 'git node-pinboard test' } ] }
-	// });
-
-function microLinkArray(result){
-  var array = [];
-  var as = JSON.parse(result);
-  as.forEach(function(posts){
-    array.push(posts.href);
-  });
+function readOPML(TheOPMLFeed){
+		opmlRead.readOpmlFile(TheOPMLFeed, function(opmlFeed){
+		if (opmlFeed == undefined) {
+			console.log(err);
+			}
+		else {
+			for (var i = 0; i < opmlFeed.subs.length; i++) {
+				 readFeed(opmlFeed.subs[i].xmlurl)
+				}
+			}
+	});
 }
 
-pinboard.get(function(err, body){
-//  var as = JSON.parse(body.posts);
-//microLinkArray(body);
-console.log(body.posts[1].description);
-});
+function readFeed(xmlUrlFeed){
+	feedRead.parseUrl (xmlUrlFeed, timeOutSecs, function (err, theFeed) {
+	if (err) {
+		console.log (err.message);
+		}
+	else {
+		console.log("============================================");
+     console.log ("Title: " + theFeed.head.title + ".");
+     console.log("============================================");
+		for (var i = 0; i < theFeed.items.length; i++) {
+
+			// Check if an item is already in the stored array
+
+			//Loop over the rest items and store each item in the array
+				console.log(theFeed.items [i].title,': ',theFeed.items [i].link);
+			 itemArray.push({
+				 guid:theFeed.items [i].guid,
+				 title:theFeed.items [i].title,
+				 pubdate:theFeed.items [i].pubdate,
+				 link:theFeed.items [i].link,
+				 description:theFeed.items [i].description
+			 })
+			}
+			//Save the array locally
+			// console.log(itemArray);
+			fs.appendFileSync(itemsToPostFile,utils.jsonStringify (itemArray))
+		}
+	});
+}
+
+function removeFeed(TheFile){
+	fs.stat(TheFile, function(err,stat){
+		if(err == null){
+			//als bestand bestaat
+			console.log('File exists', TheFile);
+			//gooi het dan weg
+			fs.unlink(TheFile, (err) => {
+  			if (err) throw err;
+  			console.log(TheFile, 'was deleted');
+				});
+		} else if(err.code == 'ENOENT') {
+			//bestand bestaat niet
+		} else {
+				console.log('Some error:', err.code);
+		}
+	})
+	//doe anders niets
+}
+
+removeFeed(itemsToPostFile);
+readOPML(opmlTestfeed);
